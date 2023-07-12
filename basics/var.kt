fe92906c8bca7ebf5294463736c7c6b10b74ca1b
Mohd Ajlal <ajlal00786@gmail.com>
2023-07-13 02:35:30 +0530

    kotlin

---
+++ basics/var.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin.basics
+
+fun main(){
+    var myName = "ajlal"
+    print("Hello " + myName)
+    print("\n")
+    myName = "mohd"
+    print("Hello " + myName)
+}
+
+// var can change it value but val can't change it value.
