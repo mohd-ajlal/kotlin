97efe6560962d533235f179425d13739fed31537
Mohd Ajlal <ajlal00786@gmail.com>
2023-07-14 03:17:05 +0530

    Kotlin

---
+++ basics/stringInterpolation.kt
@@ -0,0 +1,8 @@
+package com.example.kotlin.basics
+
+fun main(){
+    var myName = "Mohd Ajlal"
+    print("Hello $myName")  //String Interpolation
+    print("\n")
+    print("Length of myName is ${myName.length}")
+}
