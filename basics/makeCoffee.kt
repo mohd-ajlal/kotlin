2fcb70caa48bcd7ba92575cf7c6654e56781c5b2
Mohd Ajlal <ajlal00786@gmail.com>
2023-10-13 10:45:17 +0530

    Create makeCoffee.kt

---
+++ basics/makeCoffee.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin.basics
+
+fun main(){
+    // call function
+    coffee()
+}
+
+fun coffee(){
+    // define function
+
+    println("Buy beans")
+    println("Grind beans")
+    println("Hot water to 100°C")
+    println("Add water")
+    println("Put hot water through the grinded beans")
+}
